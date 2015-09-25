2c6e830eb6e6a853ee8d92efb07ef3eb105424f6
Richik SC <arko03@yahoo.com>
2015-09-25 16:06:47 +0000

    added motor functionality, TeleOp should be functional

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ETeleOp.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ETeleOp.java
@@ -51,12 +51,50 @@ public class ETeleOp extends OpMode {
     float right = throttle - direction;
     float left = throttle + direction;
     
+    right = Range.clip(right, -1, 1); // Clip values so they do not exceed 1 or -1
+    left = Range.clip(left, -1, 1); // ''
+    
+    right = (float)scaleInput(right);
+    left = (float)scaleInput(left);
+    
+    motorRight.setPower(right);
+    motorLeft.setPower(left);
+    
   }
   
   @Override // 26
   public void stop() {
-    
+    telemetry.addData("Stop", "Robot Stopped");
   }
-  
-  
+  /* Copied from K9TeleOp.java
+  Copyright (c) 2014  Qualcomm Technologies Inc
+  */
+  private double scaleInput(double dVal) {
+    double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+				0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
+			
+		// get the corresponding index for the scaleInput array.
+		int index = (int) (dVal * 16.0);
+
+		// index should be positive.
+		if (index < 0) {
+			index = -index;
+		}
+
+		// index cannot exceed size of array minus 1.
+		if (index > 16) {
+			index = 16;
+		}
+
+		// get value from the array.
+		double dScale = 0.0;
+		if (dVal < 0) {
+			dScale = -scaleArray[index];
+		} else {
+			dScale = scaleArray[index];
+		}
+
+		// return scaled value.
+		return dScale;
+  }
 }
