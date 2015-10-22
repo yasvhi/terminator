2758ad4188ef8a7ce92152f2c245b930d754cbcd
Richik SC <arko03@yahoo.com>
2015-10-22 14:31:06 -0500

    Clip the ranges

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/customCode/TeleOp.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/customCode/TeleOp.java
@@ -2,6 +2,7 @@ package com.qualcomm.ftcrobotcontroller.opmodes.customCode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
 
 
 public class TeleOp extends OpMode {
@@ -20,7 +21,9 @@ public class TeleOp extends OpMode {
     public void loop() {
         float leftY = gamepad1.left_stick_y;
         float rightY = gamepad1.right_stick_y;
-
+        
+        leftY = Range.clip(leftY, -1, 1);
+        rightY = Range.clip(rightY, -1, 1);
         motorRight.setPower(rightY);
         motorLeft.setPower(leftY);
     }
