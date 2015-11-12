c5e627fab543e18e47dba2f3aca10a67d65c2d03
Richik SC <arko03@yahoo.com>
2015-11-12 17:45:19 +0000

    Update to OPMIA and new format

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/customCode/Autonomous.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/customCode/Autonomous.java
@@ -17,48 +17,58 @@ public class Autonomous extends LinearOpMode {
     final static double COUNTS = ENCODER_CPR * ROTATIONS * GEAR_RATIO;
     final static int ERROR_THRESHOLD = 10;
 
+    private void etSetup() throws InterruptedException {
+      right = hardwareMap.dcMotor.get("Right");
+      left = hardwareMap.dcMotor.get("Left");
+      right.setDirection(DcMotor.Direction.REVERSE);
+
+      right.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
+      left.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
+      telemetry.addData("Reset Encoders", "Done");
+      //distance = 12;
+      right.setTargetPosition((int) COUNTS);
+      left.setTargetPosition((int) COUNTS);
+
+      right.setChannelMode(DcMotorController.RunMode.RUN_TO_POSITION);
+      left.setChannelMode(DcMotorController.RunMode.RUN_TO_POSITION);
+      telemetry.addData("Running to Target", "Started");
+
+      telemetry.addData("Motor Target", COUNTS);
+      telemetry.addData("Left Position", left.getCurrentPosition());
+      telemetry.addData("Right Position", right.getCurrentPosition());
+
+      right.setPower(0.5);
+      left.setPower(0.5);
+      telemetry.addData("Setting Power", "Set");
+
+      telemetry.addData("Motor Target", COUNTS);
+      telemetry.addData("Left Position", left.getCurrentPosition());
+      telemetry.addData("Right Position", right.getCurrentPosition());
+    }
+    
+    private void etLoop() throws InterruptedException {
+      telemetry.addData("Motor Target", COUNTS);
+      telemetry.addData("Left Position", left.getCurrentPosition());
+      telemetry.addData("Right Position", right.getCurrentPosition());
+      int error = Math.abs(right.getCurrentPosition()) - (int) COUNTS;
+
+      if (error < ERROR_THRESHOLD) {
+          right.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
+          left.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
+          telemetry.addData("Final Reset", "Done");
+          break;
+      }
+      //waitOneHardwareCycle();
+    }
+    
     @Override
     public void runOpMode() throws InterruptedException {
-        right = hardwareMap.dcMotor.get("Right");
-        left = hardwareMap.dcMotor.get("Left");
-        right.setDirection(DcMotor.Direction.REVERSE);
-
-        right.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
-        left.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
-        telemetry.addData("Reset Encoders", "Done");
-        //distance = 12;
-        right.setTargetPosition((int) COUNTS);
-        left.setTargetPosition((int) COUNTS);
-
-        right.setChannelMode(DcMotorController.RunMode.RUN_TO_POSITION);
-        left.setChannelMode(DcMotorController.RunMode.RUN_TO_POSITION);
-        telemetry.addData("Running to Target", "Started");
-
-        telemetry.addData("Motor Target", COUNTS);
-        telemetry.addData("Left Position", left.getCurrentPosition());
-        telemetry.addData("Right Position", right.getCurrentPosition());
-
-        right.setPower(0.5);
-        left.setPower(0.5);
-        telemetry.addData("Setting Power", "Set");
-
-        telemetry.addData("Motor Target", COUNTS);
-        telemetry.addData("Left Position", left.getCurrentPosition());
-        telemetry.addData("Right Position", right.getCurrentPosition());
-
-        while (true) { // && left.getCurrentPosition() == DISTANCE) {
-            telemetry.addData("Motor Target", COUNTS);
-            telemetry.addData("Left Position", left.getCurrentPosition());
-            telemetry.addData("Right Position", right.getCurrentPosition());
-            int error = Math.abs(right.getCurrentPosition()) - (int) COUNTS;
-
-            if (error < ERROR_THRESHOLD) {
-                right.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
-                left.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
-                telemetry.addData("Final Reset", "Done");
-                break;
-            }
-            //waitOneHardwareCycle();
+      
+        etSetup();
+        while (opModeIsActive()) { // && left.getCurrentPosition() == DISTANCE) {
+        
+          etLoop();
+          
         }
 
     }
