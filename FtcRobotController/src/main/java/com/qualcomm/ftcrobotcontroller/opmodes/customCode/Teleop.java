550dc2574986842fe720accbbc0c4015df063c23
Luke Langefels <luke.langefels@comcast.net>
2015-11-14 10:07:17 -0600

    added toggle to Teleop

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/customCode/Teleop.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/customCode/Teleop.java
@@ -25,10 +25,20 @@ public class Teleop extends OpMode {
     public void loop() {
         float leftY = -gamepad1.left_stick_y;
         float rightY = -gamepad1.right_stick_y;
+        boolean toggled = false;
+
+        if(gamepad1.x == true) {
+            toggled = !toggled;
+        }
+        if(toggled) {
+            leftY = gamepad1.left_stick_y;
+            rightY = gamepad1.right_stick_y;
+        }
+        if(gamepad1.b == true) {
+            toggled = !toggled;
+        }
 
         Right.setPower(rightY);
         Left.setPower(leftY);
-        //backRight.setPower(rightY);
-        //backLeft.setPower(leftY);
     }
 }
