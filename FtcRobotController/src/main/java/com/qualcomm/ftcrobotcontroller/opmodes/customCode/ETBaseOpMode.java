7808df4e50842f30f6b2ff5413bc68648a48feaa
Luke Langefels <luke.langefels@comcast.net>
2015-11-14 10:22:33 -0600

    Start tracking ETBaseOpMode

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/customCode/ETBaseOpMode.java
@@ -0,0 +1,33 @@
+package com.qualcomm.ftcrobotcontroller.opmodes.customCode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class ETBaseOpMode extends LinearOpMode {
+  private boolean loopBreaker;
+  public void etInit() throws InterruptedException {
+    
+  }
+  public void etSetup() throws InterruptedException {
+    
+  }
+  public void etLoop() throws InterruptedException {
+    
+  }
+  public void etCleanup() throws InterruptedException {
+
+  }
+
+  @Override
+  public void runOpMode()  throws InterruptedException {
+    etInit();
+    waitForStart();
+    etSetup();
+    while(opModeIsActive()) {
+      etLoop();
+      if(loopBreaker) {
+        break;
+      }
+      etCleanup();
+    }
+  }
+}
