0df64fea7435f30a4bb54ca80f98c7bf47263cbc
Luke Langefels <luke.langefels@comcast.net>
2015-11-17 17:21:56 -0600

    added servo control in teleop

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/customCode/Teleop.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/customCode/Teleop.java
@@ -2,11 +2,14 @@ package com.qualcomm.ftcrobotcontroller.opmodes.customCode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 
 public class Teleop extends OpMode {
     private DcMotor Right;
     private DcMotor Left;
+    private Servo servoR;
+    private Servo servoL;
     //private DcMotor backRight;
     //private DcMotor backLeft;
 
@@ -14,11 +17,14 @@ public class Teleop extends OpMode {
     public void init() {
         Right = hardwareMap.dcMotor.get("Right");
         Left = hardwareMap.dcMotor.get("Left");
+        servoR = hardwareMap.servo.get("ServoR");
+        servoL = hardwareMap.servo.get("ServoL");
         //backRight = hardwareMap.dcMotor.get("Back_Right");
         //backLeft = hardwareMap.dcMotor.get("Back_Left");
 
         Right.setDirection(DcMotor.Direction.REVERSE);
         //backRight.setDirection(DcMotor.Direction.REVERSE);
+
     }
 
     @Override
@@ -27,6 +33,7 @@ public class Teleop extends OpMode {
         float rightY = -gamepad1.right_stick_y;
         boolean toggled = false;
         double factor = 0.25;
+        double servoPosition = 0;
 
         if(gamepad1.right_bumper || gamepad1.left_bumper) {
             toggled = true;
@@ -41,7 +48,27 @@ public class Teleop extends OpMode {
             factor = gamepad1.right_trigger;
 
         telemetry.addData("Power Factor", factor);
+        telemetry.addData("Servo", servoPosition);
         Right.setPower(rightY * factor);
         Left.setPower(leftY * factor);
+
+        if(gamepad1.b) {
+          if(gamepad1.dpad_up) {
+            servoPosition++;
+          }
+          else if(gamepad1.dpad_down) {
+            servoPosition--;
+          }
+          servoR.setPosition(servoPosition);
+        }
+        if(gamepad1.x) {
+          if ((gamepad1.dpad_up)) {
+            servoPosition++;
+          }
+          else if(gamepad1.dpad_down) {
+            servoPosition--;
+          }
+          servoL.setPosition(servoPosition);
+        }
     }
 }
