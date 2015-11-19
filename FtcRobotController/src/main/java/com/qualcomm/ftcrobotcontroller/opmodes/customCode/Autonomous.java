fcf291108556ea1af8dbe1e028a7491740531f9b
Richik SC <arko03@yahoo.com>
2015-11-19 17:52:49 -0600

    Update Autonomous, easy to read, opmode.stop

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/customCode/Autonomous.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/customCode/Autonomous.java
@@ -16,22 +16,93 @@ public class Autonomous extends ETBaseOpMode {
   double counts;
   final static int ERROR_THRESHOLD = 10;
 
+  private static boolean isTargetSet = false;
+
   @Override
   public void etSetup() throws InterruptedException {
     right = hardwareMap.dcMotor.get("Right");
     left = hardwareMap.dcMotor.get("Left");
     right.setDirection(DcMotor.Direction.REVERSE);
 
-    this.counts = getCountsForDistance(distance);
+  }
 
+
+
+  @Override
+  public void etLoop() throws InterruptedException {
+    telemetry.addData("Left Position", left.getCurrentPosition());
+    telemetry.addData("Right Position", right.getCurrentPosition());
+    telemetry.addData("Right Motor Power", Double.toString(right.getPower()));
+    telemetry.addData("Left Motor Power", Double.toString(left.getPower()));
+    telemetry.addData("Motor Target", counts);
+
+
+
+    // go 20 and then stop
+      // setTarget for 20
+
+    setTarget(20);
+    // goToTarget()
+    goToTarget();
+    // loop until 20 is reached
+    if(!hasArrived(20))
+      return;
+    telemetry.addData("hasArrived 20", "true");
+
+    // go to 10 and then stop
+    setTarget(10);
+    // goToTarget()
+    goToTarget();
+    // loop until 20 is reached
+    if(!hasArrived(10))
+      return;
+    telemetry.addData("hasArrived 10", "true");
+    stopRobot();
+
+
+  }
+
+  private boolean hasArrived(int dist) {
+    double cts = getCountsForDistance(dist);
+    int rightErrorMargin = Math.abs(Math.abs(right.getCurrentPosition()) - (int) cts);
+    int leftErrorMargin = Math.abs(Math.abs(right.getCurrentPosition()) - (int) cts);
+    if (leftErrorMargin < ERROR_THRESHOLD && rightErrorMargin < ERROR_THRESHOLD) {
+      isTargetSet = false;
+      return true;
+    }
+    return false;
+
+  }
+
+  private void stopRobot() throws InterruptedException {
+    right.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+    left.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+    etBreakLoop();
+  }
+
+  private double getCountsForDistance(int distance) {
+    double rotations = distance / CIRCUMFERENCE;
+    return ENCODER_CPR * rotations * GEAR_RATIO;
+  }
+
+  private void setTarget(int distance) {
+    if (isTargetSet) {
+      return;
+    }
+    double counts = getCountsForDistance(distance);
     right.setMode(DcMotorController.RunMode.RESET_ENCODERS);
     left.setMode(DcMotorController.RunMode.RESET_ENCODERS);
     telemetry.addData("Reset Encoders", "Done");
+
     right.setTargetPosition((int) counts);
     left.setTargetPosition((int) counts);
-
     right.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
     left.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
+    isTargetSet = true;
+  }
+
+  private void goToTarget() {
+
     telemetry.addData("Running to Target", "Started");
 
     telemetry.addData("Motor Target", counts);
@@ -41,53 +112,5 @@ public class Autonomous extends ETBaseOpMode {
     right.setPower(0.25);
     left.setPower(0.25);
     telemetry.addData("Power Set", "true");
-
-    //telemetry.addData("Motor Target", COUNTS);
-    //telemetry.addData("Left Position", left.getCurrentPosition());
-    //telemetry.addData("Right Position", right.getCurrentPosition());
-  }
-  
-  @Override
-  public void etLoop() throws InterruptedException {
-    telemetry.addData("Left Position", left.getCurrentPosition());
-    telemetry.addData("Right Position", right.getCurrentPosition());
-    telemetry.addData("Right Motor Power", Double.toString(right.getPower()));
-    telemetry.addData("Left Motor Power", Double.toString(left.getPower()));
-    telemetry.addData("Motor Target", counts);
-
-    //int error = Math.abs(right.getCurrentPosition()) - (int) COUNTS;
-
-    if(!hasArrived(20))
-      return;
-    telemetry.addData("hasArrived 20", "true");
-    stopRobot();
-    this.distance = 30;
-    if (hasArrived(30)) {
-      telemetry.addData("hasArrived 30", "true");
-      stopRobot();
-      telemetry.addData("Robot Stopped", "true");
-      etBreakLoop();
-
-    }
-  }
-
-  private boolean hasArrived(int dist) {
-    double cts = getCountsForDistance(dist);
-    int rightErrorMargin = Math.abs(Math.abs(right.getCurrentPosition()) - (int) cts);
-    int leftErrorMargin = Math.abs(Math.abs(right.getCurrentPosition()) - (int) cts);
-    if (leftErrorMargin < ERROR_THRESHOLD && rightErrorMargin < ERROR_THRESHOLD)
-      return true;
-    return false;
-
-  }
-
-  private void stopRobot() {
-    right.setMode(DcMotorController.RunMode.RESET_ENCODERS);
-    left.setMode(DcMotorController.RunMode.RESET_ENCODERS);
-  }
-
-  private double getCountsForDistance(int distance) {
-    double rotations = distance / CIRCUMFERENCE;
-    return ENCODER_CPR * rotations * GEAR_RATIO;
   }
 }
